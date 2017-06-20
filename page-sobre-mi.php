f5aeaf4f66883bee616f7ee0fba4c6aeddcf9fb7
Michel <pmichven@gmail.com>
2017-06-20 13:31:07 -0500

    Frontpage Mich headshot

---
+++ page-sobre-mi.php
@@ -0,0 +1,8 @@
+<?php
+
+//* Remove header markup and page title
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+
+genesis();
